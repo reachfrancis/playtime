cf5ca852383131586107fec88d7480a6ca991ef0
reachfrancis@verizon.net <reachfrancis@verizon.net>
2020-10-27 16:01:42 -0400

    adds foo.cs

---
+++ foo.cs
@@ -0,0 +1,11 @@
+ Volume in drive C is Windows
+ Volume Serial Number is A6F2-5160
+
+ Directory of C:\Users\reach\playRepo\playtime
+
+10/27/2020  04:01 PM    <DIR>          .
+10/27/2020  04:01 PM    <DIR>          ..
+10/27/2020  04:01 PM                 0 foo.cs
+10/27/2020  03:53 PM               354 readme.txt
+               2 File(s)            354 bytes
+               2 Dir(s)  55,374,561,280 bytes free
